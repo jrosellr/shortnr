13962d75109753a9f3ee9eb89a6676f36b154768
Joan Rosell <joan.rosellr@gmail.com>
2023-12-18 15:52:29 +0100

    feat: add shortener service implementation

---
+++ apps/shortnr-service/shortnr.WebApi/Program.cs
@@ -0,0 +1,23 @@
+using System.Text.Json.Serialization;
+using shortnr.WebApi.Endpoints;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
+
+var app = builder.Build();
+
+app.MapGroup("/api")
+  .MapPost("/shorten", Endpoints.Shorten);
+
+app.MapGet("/{*slug}", Endpoints.Redirect);
+
+app.Run();
+
+
+[JsonSerializable(typeof(ShortenResponse))]
+[JsonSerializable(typeof(ShortenRequest))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext { }
