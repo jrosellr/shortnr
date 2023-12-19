8fa7555f31a5331009badce14ec844158fc7894b
Joan Rosell <joan.rosellr@gmail.com>
2023-12-19 15:54:50 +0100

    feat: add docker compose support
    * feat: add Docker support for the web api
    
    * feat: add basic docker compose support
    
    * feat: add launcher script to run docker compose

--- apps/shortnr-service/shortnr.WebApi/Program.cs
+++ apps/shortnr-service/shortnr.WebApi/Program.cs
@@ -3,6 +3,8 @@ using shortnr.WebApi.Endpoints;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
+builder.Services.AddHealthChecks();
+
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
@@ -12,6 +14,8 @@ var app = builder.Build();
 
 app.MapGroup("/api")
   .MapPost("/shorten", Endpoints.Shorten);
+
+app.MapHealthChecks("/health");
 
 app.MapGet("/{*slug}", Endpoints.Redirect);
 
